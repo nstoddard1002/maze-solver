301bc2cd037d1d9875312615836117b7a97905c7
nstoddard <nstoddard@proton.me>
2024-11-30 17:36:25 -0500

    added draw_move() method to the cell class

--- window.py
+++ window.py
@@ -44,9 +44,56 @@ class Line():
         self.p1 = point_2
         self.p2 = point_2
 
-    def draw(self, canvas, fill_color):
+    def draw(self, canvas, fill_color="black"):
 
         canvas.create_line(self.p1.x,self.p1.y,self.p2.x,self.p2.y,fill=fill_color,width=2)
+
+class Cell():
+    def __init__(self,window):
+        self.has_left_wall=True
+        self.has_right_wall=True
+        self.has_top_wall=True
+        self.has_bottom_wall=True
+        
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+
+        self._win = window
+
+    def draw(self,x1,y1,x2,y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        
+        if self.has_left_wall:
+            line = Line(Point(x1,y1), Point(x2,y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1,y2), Point(x2,y2))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2,y2), Point(x2,y1))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x2,y1), Point(x1,y1))
+            self._win.draw_line(line)
+    
+    def draw_move(self, to_cell, undo=False):
+        color = "red"
+        if undo:
+            color = "gray"
+        
+        start_x = (self._x1 + self._x2) / 2
+        start_y = (self._y1 + self._y2) / 2
+
+        end_x = (to_cell._x1 + to_cell._x2) / 2
+        end_y = (to_cell._y1 + to_cell._y2) / 2
+
+        line = Line(Point(start_x,start_y),Point(end_x,end_y))
+        self._win.draw_line(line,color)
 
         
         
