c7af7ec5d14e486712b2f196acda3b9720100d60
nstoddard <nstoddard@proton.me>
2024-11-30 23:59:33 -0500

    added maze class and assorted methods

--- maze.py
+++ maze.py
@@ -1,9 +1,14 @@
 from tkinter import Tk, BOTH, Canvas
+import time
+
 
 class Window():
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
+
+        self._width = width
+        self._height = height
 
         #these were using grid
         #self.root.columnconfigure(0,weight=1)
@@ -62,6 +67,9 @@ class Cell():
         self._win = window
 
     def draw(self,x1,y1,x2,y2):
+        if self._win is None:
+            return
+
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
@@ -85,14 +93,75 @@ class Cell():
         if undo:
             color = "gray"
         
-        start_x = (self._x1 + self._x2) / 2
-        start_y = (self._y1 + self._y2) / 2
+        start_x = abs(self._x1 + self._x2) // 2
+        start_y = abs(self._y1 + self._y2) // 2
 
-        end_x = (to_cell._x1 + to_cell._x2) / 2
-        end_y = (to_cell._y1 + to_cell._y2) / 2
+        end_x = abs(to_cell._x1 + to_cell._x2) // 2
+        end_y = abs(to_cell._y1 + to_cell._y2) // 2
 
         line = Line(Point(start_x,start_y),Point(end_x,end_y))
         self._win.draw_line(line,color)
+
+class Maze():
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win
+    ):
+        self.maze_x = x1
+        self.maze_y  = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.size_x = cell_size_x
+        self.size_y = cell_size_y
+        self.win = win
+
+        self._create_cells()
+
+    def _create_cells(self):
+        self._cells = []
+        for i in range(self.num_rows):
+            row_cells = []
+            for j in range(self.num_cols):
+                row_cells.append(Cell(self.win))
+            self._cells.append(row_cells)
+        
+        maze_width = num_cols * self.size_x
+        maze_height = num_rows * self.size_y
+
+        if maze_width > (self.win._width - self.maze_x):
+            raise Exception("maze too wide for window")
+        
+        if maze_height > (self.win._height - self.maze_y):
+            raise Exception("maze too tall for window")
+        
+        for k in range(0,self.num_rows):
+            for l in range(0,self.num_cols):
+                self._draw_cell(k,l)
+    
+    def _draw_cell(self,i,j):
+        cell_x1 = self.size_x * i 
+        cell_y1 = self.size_y * j
+        cell_x2 = self.size_x * (i + 1)
+        cell_y2 = self.size_y * (j + 1)
+
+        cell_x1 += self.maze_x
+        cell_y1 += self.maze_y
+        cell_x2 += self.maze_x
+        cell_y2 += self.maze_y
+
+        self._cells[i][j].draw(cell_x1,cell_y1,cell_x2,cell_y2)
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
+        
+        
 
         
         
